a620f54b6c3e67db182bab9b1c96d25d24e01a35
Skoles27 <Skoles27@gmail.com>
2024-04-27 20:26:44 +0200

    Init

---
+++ src/main/java/com/skoles/socialN/SocialNApplication.java
@@ -0,0 +1,13 @@
+package com.skoles.socialN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skoles/socialN/SocialNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skoles.socialN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
